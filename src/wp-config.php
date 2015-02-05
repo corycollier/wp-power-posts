95a4dd820d78a98b779332a5c3acd0932a79d3f8
Cory Collier <corycollier@corycollier.com>
2015-02-04 22:00:15 -0500

    adding potential files to ignore, and updating the configuration file

--- src/wp-config.php
+++ src/wp-config.php
@@ -14,21 +14,48 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+// specify the known environments here
+$environments = array(
+    'production'    => array(
+        'DB_HOST'     => 'localhost',
+        'DB_NAME'     => 'wp_power_posts',
+        'DB_USER'     => 'wp_power_posts',
+        'DB_PASSWORD' => 'wp-power-posts-password',
+        'DB_CHARSET'  => 'utf8',
+        'DB_COLLATE'  => '',
+        'WP_DEBUG'    => false,
+    ),
 
-/** MySQL database username */
+    'staging'   => array(
+        'DB_HOST'     => 'localhost',
+        'DB_NAME'     => 'wp_power_posts_staging',
+        'DB_USER'     => 'wp_power_posts',
+        'DB_PASSWORD' => 'wp-power-posts-password',
+        'DB_CHARSET'  => 'utf8',
+        'DB_COLLATE'  => '',
+        'WP_DEBUG'    => false,
+    ),
 
-/** MySQL database password */
+    'development'   => array(
+        'DB_HOST'     => '127.0.0.1',
+        'DB_NAME'     => 'wp_power_posts',
+        'DB_USER'     => 'wp_power_posts',
+        'DB_PASSWORD' => 'wp-power-posts-password',
+        'DB_CHARSET'  => 'utf8',
+        'DB_COLLATE'  => '',
+        'WP_DEBUG'    => true,
+    ),
 
-/** MySQL hostname */
-define('DB_HOST', 'mysqlv113');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+    'qa'        => array(
+        'DB_HOST'     => 'localhost',
+        'DB_NAME'     => 'wp_power_posts',
+        'DB_USER'     => 'wp_power_posts',
+        'DB_PASSWORD' => 'wp-power-posts-password',
+        'DB_CHARSET'  => 'utf8',
+        'DB_COLLATE'  => '',
+        'WP_DEBUG'    => true,
+    ),
+);
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -57,7 +84,6 @@ $table_prefix  = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -67,15 +93,35 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 define('FS_METHOD', 'direct');
-define('AUTH_KEY',         'k3dO3=2i_h?5E(-x|+mmo)X0AYHE)2&4 wz*g*O5Kh-2.Y[cIFvI;l;;v*Ws@1X(');
-define('SECURE_AUTH_KEY',  '}`uL+vU9yG[uueTY<BL3qV+0l2;l,#igf;+0+V UBl!Ktrr8RAUoJyitr41$_1cC');
-define('LOGGED_IN_KEY',    '=oG<9~cdmQ_tkEd+J^/:3YW)`Ce~([A  AL,Hr1]uQ=Ca pq:a%O&tF-CGQ*#=o9');
-define('NONCE_KEY',        'mV*7LjL-7GwU^_#KTm&_1GolUqmln-P--bQl(t/P<0f+AD[cSotY7I1bmWW#zsFW');
-define('AUTH_SALT',        'lK.iUWBy,vl[dqoBpE;(oW9B,66g^c|c5voO{1@7oJC8PVvsd5{-k#}X$Z:F$Lju');
-define('SECURE_AUTH_SALT', '] x)($b1+i|236lg3p)0DPm j /#/w+$nKN_(ay_B{X/6E`]&!-/qg^&ue_w2#6r');
-define('LOGGED_IN_SALT',   'J|1g{.6=2:n=6!%K;6HT4vp_biPx(4t^Qfba&/Nj-:$cf2_/6&vRPo|v-|Tx.1tA');
-define('NONCE_SALT',       '(@wPsz}j>0@On =His,|,o1bUVAJZ0qNFOoNr1@@Bs[]60fCm^iSWn3f{3^MHe|1');
-define('DB_NAME', 'light_up_viera');
-define('DB_USER', 'evolve1');
-define('DB_PASSWORD', 'u3Esc23dk_di!');
+define('AUTH_KEY',         'g|F )`k~hny}<hPKn7ek{P%SR-<R@fkso R/?;{n.]WLAt.A4C;=@GP*8|M$G,3Y');
+define('SECURE_AUTH_KEY',  '54r/F2B@*[BYS}gTZ?eT^b|Wjkd#VX_II{IvOCud4G$2#9={0NFXQwkT,uBnO89^');
+define('LOGGED_IN_KEY',    '.8nf]5KXdW%ioS-S$LMBOSZvs%W-oZkP5N- *}}o6*U|7k!,T+|3J[Oy/G%b~0v(');
+define('NONCE_KEY',        'M`+)YSwOIA`di|@G4};qrRqZ3o@KBmg!N[Lx/Mm[$%#GRzVs;m{e|?o+%0-8+uOk');
+define('AUTH_SALT',        'QvzTDB,]=nP0J7e-@z+H/Y~}9+z#iwtu3j%y)>KHX*>#~K*N$.0i2!]zg1-OUp,(');
+define('SECURE_AUTH_SALT', '<ca:Mdt+HoG:2FT/l>.M1n7|o}ws-_ko]2v/({.=@A0WHFH/<qwU)?SU[u$K<5=&');
+define('LOGGED_IN_SALT',   'e>NnnE68YF88)]_I>U^ZnUS;$FjR8skb/|#F&[~<BCD7$GCrF&{Y5R6t;k/V.YT(');
+define('NONCE_SALT',       'Q8<5mc$s8TE):9jK~bUakmEa/F (G]it_5Aiv#;8CuF-82 :aY?,va8>bBYC=3*a');
+
+
+// determine which environment is being served. Default to produciton
+$env = getenv('APP_ENV');
+if (! $env) $env = 'production';
+$env = $environments[$env];
+
+// ** MySQL settings - You can get this info from your web host ** //
+define('DB_HOST',     $env['DB_HOST']);
+define('DB_CHARSET',  $env['DB_CHARSET']);
+define('DB_COLLATE',  $env['DB_COLLATE']);
+define('DB_NAME',     $env['DB_NAME']);
+define('DB_USER',     $env['DB_USER']);
+define('DB_PASSWORD', $env['DB_PASSWORD']);
+
+define('WP_DEBUG',    $env['WP_DEBUG']);
+define('WP_DEBUG_LOG',$env['WP_DEBUG']);
+
+// avoid database values, as this is a huge pain
+define('WP_SITEURL',   'http://' . $_SERVER['SERVER_NAME'] . '/');
+define('WP_HOME',      'http://' . $_SERVER['SERVER_NAME'] . '/');
+
+
 require_once(ABSPATH . 'wp-settings.php');
